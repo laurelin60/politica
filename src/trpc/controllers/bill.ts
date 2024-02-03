126dbbe94905bc21dcb335bc3f53c687391ef53a
uno-p-5 <wpasadhika@gmail.com>
2024-02-03 11:09:04 -0800

    a

--- src/trpc/controllers/bill.ts
+++ src/trpc/controllers/bill.ts
@@ -13,13 +13,11 @@ export const getBillById = publicProcedure.input(z.object({ billId: z.string() }
 
 export const getLegislatorBills = publicProcedure
   .input(z.object({
-    billId: z.string(),
     authorNameContains: z.string(), // Add this line to accept author name as a parameter
   }))
   .query(async (opts) => {
     return db.bill.findMany({
       where: {
-        billId: opts.input.billId,
         author: {
           // Perform a join with the Legislator table and filter by author's name
           name: {
