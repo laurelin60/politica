0e3f5e75a8e96fa9d98adeb586c908ba2c009021
Kevin Wu <kevinwu098@gmail.com>
2024-02-03 11:16:19 -0800

    feat: finish off chat scaffold

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-// import ActionCard from "@/components/ActionCard";
-// import ActionItem from "@/components/ActionItem";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-// import { SelfLoveTag } from "@/components/ui/tag";
 import { useChat } from "ai/react";
 import { Loader2, Mic, Send } from "lucide-react";
 
@@ -71,37 +68,43 @@ const Chat = () => {
         <div className="h-full flex justify-between gap-x-8 w-full">
             <div className="flex-col w-full relative mb-8">
                 <ScrollArea
-                    className="flex flex-col space-y-4 h-[700px] min-w-full max-w-fit py-2 pr-4 bg-white rounded-4xl"
+                    className="flex flex-col space-y-4 h-[670px] min-w-full max-w-fit py-2 pr-4 bg-white rounded-4xl"
                     id="messageContainer"
                 >
                     <div
-                        className="space-y-4 justify-start items-start flex flex-col w-full"
+                        className="space-y-8 justify-start items-start flex flex-col w-full py-4"
                         ref={ref}
                     >
                         {messages.map((m, index) => (
                             <div key={m.id}>
                                 {m.role === "user" ? (
                                     <div className="space-y-4">
-                                        <div className="bg-jas-grey_light rounded-xl p-4 flex flex-col gap-y-2">
+                                        <div className="rounded-xl flex flex-col gap-y-2">
                                             <div className="flex items-center gap-x-2 flex-row">
-                                                <Avatar className="size-8">
+                                                <Avatar className="size-10">
                                                     <AvatarImage
                                                         src="https://github.com/shadcn.png"
                                                         alt="@shadcn"
                                                     />
                                                     <AvatarFallback>
-                                                        M
+                                                        Y
                                                     </AvatarFallback>
                                                 </Avatar>
-                                                <span className="font-bold text-xl">
-                                                    you
+                                                <span className="font-bold text-2xl">
+                                                    You
                                                 </span>
                                             </div>
-                                            <div className="text-black text-opacity-50 font-semibold flex-nowrap break-words max-w-[400px]">
+                                            <div className="p-8 rounded-4xl border-4 border-[#817D88] text-white bg-[#817D88]">
+                                                <p className="font-semibold text-xl">
+                                                    where can I find support to
+                                                    access HRT?
+                                                </p>
+                                            </div>
+                                            {/* <div className="text-black text-opacity-50 font-semibold flex-nowrap break-words max-w-[400px]">
                                                 <span className="min-w-0">
                                                     {m.content}
                                                 </span>
-                                            </div>
+                                            </div> */}
                                         </div>
 
                                         {index == messages.length - 1 &&
