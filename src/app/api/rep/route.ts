2b57208427217c585cf223daf0da839c1d395f92
Andrew Wang <awwang3@uci.edu>
2024-02-03 17:50:03 -0800

    schema breaking

--- src/app/api/rep/route.ts
+++ src/app/api/rep/route.ts
@@ -1,4 +1,6 @@
-export function GET(req, res) {
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(req: NextRequest, res: NextResponse) {
     const data = { name: "haha", desc: "desc" };
 
     return res.json(data);
